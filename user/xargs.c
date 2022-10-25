84c23be9775947d360798eabab7673f2f7bdefa2
Juraj Revaj <juraj.revaj14@gmail.com>
2022-10-25 21:21:01 +0200

    util: xargs fix + uptime

--- user/xargs.c
+++ user/xargs.c
@@ -1,81 +1,48 @@
-#include "kernel/param.h"
 #include "kernel/types.h"
 #include "user/user.h"
+#include "kernel/param.h"
 
-int
-fmtpargs(char **pargs, char *buf, int n)
-{
-  int t = 0;
-  char *h = buf + 1, *p = buf + 1;
-  for (int i = 1; i < n; ++i) {
-    if (buf[i] == '\\' || buf[i] == '\"') {
-      pargs[t] = malloc(p - h);
-      memcpy(pargs[t++], h, p++ - h);
-      h = ++p;
-      ++i;
-    } else {
-      ++p;
-    }
-  }
-  return t;
-}
+#define NULL ((char*)0) 
 
-void
-fmtsargs(char **sargs, int at, char *buf, int n)
-{
-  char *h = buf, *p = buf;
-  for (int i = 0; i <= n; ++i) {
-    if ((buf[i] == ' ' && (p - h)) || buf[i] == '\n') {
-      sargs[at] = malloc(p - h);
-      memcpy(sargs[at++], h, p - h);
-      h = ++p;
-    } else if (buf[i] == ' ') {
-      h = ++p;
-    } else {
-      ++p;
-    }
-  }
-}
 
-int
-main(int argc, char *argv[])
-{
-  char buf[512];
-  int n;
+char* readline(){
+	char buf[512], c;
+	int pos = -1;
+	while(read(0, &c, sizeof(char))){
+		if(c == '\n'){
+			break;
+		}else{
+			buf[++pos] = c;
+		}
+	}
+	if(pos == -1){
+		return NULL;
+	}
+	//need to free
+	char *p = malloc((pos+1) * sizeof(char));
+	memmove(p, buf, pos+1);
+	return p;
+}	
 
-  if ((n = read(0, buf, sizeof buf)) < 0) {
-    fprintf(2, "xargc: failed to read buf\n");
-    exit(1);
-  }
-
-  char *args[MAXARG + 1];
-  int at = 0;
-
-  if (!strcmp(argv[1], "-n")) {  // xargs -n 1 ...
-    for (int i = 3; i < argc; ++i) {
-      args[at] = malloc(sizeof argv[i]);
-      memcpy(args[at++], argv[i], sizeof argv[i]);
-    }
-    args[at] = malloc(24);  // assume max length 24
-    char *pargs[MAXARG];
-    int t = fmtpargs(pargs, buf, n);  // pargs[0...t]
-    for (int i = 0; i < t; ++i) {
-      memcpy(args[at], pargs[i], sizeof pargs[i]);
-      if (fork() == 0) {
-        exec(args[0], args);
-        exit(0);
-      } else {
-        wait(0);
-      }
-    }
-  } else {
-    for (int i = 1; i < argc; ++i) {
-      args[at] = malloc(sizeof argv[i]);
-      memcpy(args[at++], argv[i], sizeof argv[i]);
-    }
-    fmtsargs(args, at, buf, n);
-    exec(args[0], args);
-  }
-
-  exit(0);
+int main(int argc, char *argv[]){
+	if(argc < 3){
+		fprintf(2, "Usage : xargs command args...\n");
+		exit(1);
+	}
+	char *argv_new[MAXARG];
+	for(int i = 1; i < argc; i++){
+		argv_new[i-1] = argv[i];
+	}
+	char *r;
+	while((r = readline()) != NULL){
+		argv_new[argc-1] = r;
+		if(fork() == 0){
+			exec(argv_new[0], argv_new);
+			free(r);
+			exit(0);
+		}else{
+			wait(0);
+		}
+	}	
+	exit(0);
 }
