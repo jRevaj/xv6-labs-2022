617b674ef9a129baba638576fd4ccb9337718bc3
Juraj Revaj <juraj.revaj14@gmail.com>
2022-12-09 18:01:35 +0100

    thread: threads usage

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -14,6 +14,7 @@ struct entry {
   struct entry *next;
 };
 struct entry *table[NBUCKET];
+pthread_mutex_t lock[NBUCKET];            // deklarácia zámku
 int keys[NKEYS];
 int nthread = 1;
 
@@ -41,6 +42,7 @@ void put(int key, int value)
 {
   int i = key % NBUCKET;
 
+  pthread_mutex_lock(&lock[i]);       // zamkni/získaj zámok
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -54,7 +56,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-
+  pthread_mutex_unlock(&lock[i]);     // odomkni/uvoľni zámok
 }
 
 static struct entry*
@@ -62,11 +64,12 @@ get(int key)
 {
   int i = key % NBUCKET;
 
-
+  pthread_mutex_lock(&lock[i]);       // zamkni/získaj zámok
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
+  pthread_mutex_unlock(&lock[i]);     // odomkni/uvoľni zámok
 
   return e;
 }
@@ -116,6 +119,11 @@ main(int argc, char *argv[])
   assert(NKEYS % nthread == 0);
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
+  }
+
+  for (int i = 0; i < NBUCKET; i++)
+  {
+    pthread_mutex_init(&lock[i], NULL); // inicializácia zámku
   }
 
   //
